25e55d57cc4099faa952b9325dc63d808ec75b1e
Karina <kulinicheva_91@mail.ru>
2023-07-26 11:16:32 +0300

    cash servise

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,39 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+    @Test
+    public void shouldCashTestMin() {
+        int amount = 900;
+
+        int expected = 100;
+        int actual = service.remain(900);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldCashTestNormal() {
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void shouldCashTestMax() {
+        int amount = 1100;
+
+        int expected = 900;
+        int actual = service.remain(1100);
+
+        assertEquals(expected, actual);
+
+    }
+
+}
